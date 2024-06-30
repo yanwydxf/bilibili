149b52d212228a73acc43ff1b63c5a1131285398
yanwydxf <yanwydxf@163.com>
2024-06-30 09:15:51 +0800

    feat：首页静态结构

---
+++ app.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <NuxtLink to="/">首页</NuxtLink>
+    <NuxtLink to="/video">视频</NuxtLink>
+    <NuxtPage/>
+  </div>
+</template>
+<script setup lang="ts">
+// SEO 优化信息
+useSeoMeta({
+  // 网站标题
+  title: '哔哩哔哩 (゜-゜)つロ 干杯~-bilibili',
+  // 网站描述
+  description:
+    'bilibili是国内知名的在线视频弹幕网站，拥有最棒的ACG氛围，哔哩哔哩内容丰富多元，涵盖动漫、电影、二次元舞蹈视频、在线音乐、娱乐时尚、科技生活、鬼畜视频等。下载客户端还可离线下载电影、动漫。',
+  // 搜索关键词
+  keywords: 'B站,bilibili,哔哩哔哩,哔哩哔哩动画,动漫,电影,在线动漫,高清电影',
+})
+</script>
+
+<style lang="scss">
+// 基础样式
+@import './assets/styles/base.scss';
+// 字体图标
+@import './assets/styles/iconfont.scss';
+:root {
+  /* CSS 变量设置组件库主题 */
+  --van-primary-color: #fb7299 !important;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  //是否开启 ssr 服务端渲染
+  ssr:true,
+  devtools: { enabled: true },
+  //应用模块
+  modules:["@vant/nuxt"]
+})
